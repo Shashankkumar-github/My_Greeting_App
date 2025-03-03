7dd1d2fa0a6672a244d942c64a07681c7ab27cc6
Shashankkumar-github <101034283+Shashankkumar-github@users.noreply.github.com>
2025-03-03 10:49:34 +0530

    first commit

---
+++ src/main/java/com/bridgelabz/My_Greeting_App/MyGreetingAppApplication.java
@@ -0,0 +1,13 @@
+package com.bridgelabz.My_Greeting_App;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MyGreetingAppApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MyGreetingAppApplication.class, args);
+	}
+
+}
